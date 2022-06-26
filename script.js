c648107f48d178ce4229670ef3b304c73203f295
Jacob Janak <jakeathotmail@gmail.com>
2022-06-26 19:55:58 -0400

    Initial upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			firstName: 'John',
+			lastName: 'Doe',
+			email: 'johndoe@gmail.com',
+			gender: 'male',
+			picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+		}
+	},
+	methods: {
+		async getUser() {
+			const res = await fetch('https://randomuser.me/api')
+			const { results } = await res.json();
+			const user = results[0];
+
+			this.firstName = user.name.first;
+			this.lastName = user.name.last;
+			this.email = user.email;
+			this.picture = user.picture.large;
+		}
+	}
+});
+
+app.mount('#app')
